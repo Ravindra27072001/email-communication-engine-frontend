5ebd0c582caafaa04032b3a6deb04bfc7ec3b854
Ravindra Singh Rathore <rravindrasingh168@gmail.com>
2022-09-02 03:43:27 +0530

    deploying

--- src/services/signin.js
+++ src/services/signin.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const Signin = async (credentials) => {
+    // console.log("object", process.env);
     return await axios.post(`${process.env.VUE_APP_BASE_URL}/user/signin`,
         credentials,
         {
